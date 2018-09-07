5f6b0b9a4103b5c8ae646b2281a834a8d3a701be
savybrandt <savy@wonolo.com>
2018-09-07 16:00:22 -0700

    public

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'build'),
     filename: 'bundle.js',
-    publicPath: '/'
+    publicPath: '/public/'
   },
   resolve: {
     modulesDirectories: ['node_modules', 'src'],
