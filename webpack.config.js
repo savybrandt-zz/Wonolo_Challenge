b28d6af66ce924072079c77787a7b38b5787b9c3
savybrandt <savy@wonolo.com>
2018-09-07 15:10:29 -0700

    change path to public/build

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,7 @@ module.exports = {
   output: {
     path: path.join(__dirname, 'public/build'),
     filename: 'bundle.js',
-    publicPath: '/build'
+    publicPath: 'public/build'
   },
   resolve: {
     modulesDirectories: ['node_modules', 'src'],
